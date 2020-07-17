57021eef271d6113c14c50df55ee0a0b5607ed8a
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-07-17 10:14:11 +0100

    basic cmd arg test

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::{Arg, App};
+
+fn main() {
+    let matches = App::new("Simple Static Sites")
+        .version("0.1-alpha")
+        .author("Alexander McKinney <alexander.f.mckinney@durham.ac.uk>")
+        .about("Generates a website from a collection of markdown files")
+        .arg(Arg::with_name("test")
+            .short("t")
+            .long("test")
+            .help("A test argument for learning purposes.")
+            .takes_value(true)
+        )
+        .get_matches();
+
+    println!("{:}", matches);
+}
