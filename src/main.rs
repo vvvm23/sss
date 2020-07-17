fb0b9fa6a1ffbfe8ff3cf631dfe5ae3e28840213
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-07-17 10:50:01 +0100

    reading config from file test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+mod config;
 use clap::{Arg, App};
+use toml;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SiteConfig {
+    title: String,
+    style_path: String,
+}
+
+impl Default for SiteConfig {
+    fn default() -> Self {
+        SiteConfig {
+            title: "Default Site".to_string(),
+            style_path: "style/style.css".to_string(),
+        }
+    }
+}
 
 fn main() {
     let matches = App::new("Simple Static Sites")
@@ -17,4 +37,10 @@ fn main() {
         Some(v) => println!("{}", v),
         None => println!("No argument.")
     }
+
+    let site_cfg_raw = std::fs::read_to_string("sss-config.toml")
+        .expect("Failed to read file.");
+    println!("{:?}", site_cfg_raw);
+    let site_cfg: SiteConfig = toml::from_str(&site_cfg_raw).unwrap();
+    println!("{:?}", site_cfg);
 }
