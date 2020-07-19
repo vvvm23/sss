f9bcf2a35f3012695f27fb085ae1607938728bc3
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-07-19 21:35:04 +0100

    basic parsing of headings (fixed depth), code blocks and paragraphs

--- src/md.rs
+++ src/md.rs
@@ -14,33 +14,51 @@ use std::io::BufReader;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[derive(Debug)]
 pub enum MDComponent {
     Heading(u8, String),
     Paragraph(String),
     Image(String),
     Hyperlink(String),
     CodeBlock(String),
+    Empty,
+}
+
+fn parse_heading(depth: u8, text: String) -> MDComponent {
+    MDComponent::Heading(depth, text)
+}
+
+fn parse_code(text: String) -> MDComponent {
+    MDComponent::CodeBlock(text)
+}
+
+fn parse_paragraph(text: String) -> MDComponent {
+    MDComponent::Paragraph(text)
 }
 
 /// Accepts path to markdown file and returns Vec<MDComponent> representing the file
-pub fn parse_md_file(path: &str) -> std::io::Result<()> {
+pub fn parse_md_file(path: &str) -> std::io::Result<Vec<MDComponent>> {
     let f = File::open(path)?;
     let f = BufReader::new(f);
 
-    for (i, line) in f.lines().enumerate() {
-        let line = line.unwrap().to_string();
+    let mut md_vec: Vec<MDComponent> = Vec::new();
+
+    for (i, l) in f.lines().enumerate() {
+        let line = l.unwrap().to_string();
         let mut line_chars = line.chars();
         
         let c = line_chars.next();
 
-        match c {
-            Some('#') => println!("HEADER"),
-            Some(' ') => println!("CODE"), // needs 4 spaces, not a tab
-            None => println!("Empty Line"),
-            _ => println!("Unknown."),
-        }
+        let md_c = match c {
+            // A bit dirty..
+            Some('#') => parse_heading(1, line_chars.skip(1).take_while(|_| true).collect::<String>()),
+            Some(' ') => parse_code(line_chars.skip(3).take_while(|_| true).collect::<String>()), 
+            None => MDComponent::Empty,
+            _ => parse_paragraph(line),
+        };
+        md_vec.push(md_c);
 
     }
 
-    Ok(())
+    Ok(md_vec)
 }
